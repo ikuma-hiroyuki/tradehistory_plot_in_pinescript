8151ec757adb7b5783e14ccf3452ae64221bd047
ikuma-hiroyuki <chemicalbondo@gmail.com>
2021-10-13 07:47:22 +0900

    履歴の並び順を時系列に調整

--- create_pine.py
+++ create_pine.py
@@ -1,14 +1,13 @@
+import datetime
 import csv
 import operator
 import os
 import sys
 from datetime import datetime as dt
 
-target_csv = input('Input csv file path.\n').replace('"', '')
-pine_code = open('pine.txt', 'w', encoding='UTF-8')
 
-
-def SetTickerRoot(_ticker: str):
+def SetTickerRoot(_ticker: str, date: dt):
+    adjust = True
     if ('NQ' in _ticker):
         _ticker = 'NQ'
     elif ('ES' in _ticker):
@@ -21,8 +20,12 @@ def SetTickerRoot(_ticker: str):
         _ticker = 'USDJPY'
     else:
         _ticker
-    return _ticker
+        adjust = False
+    # 現物は+1日しないとTrading viewで1日ずれる
+    return _ticker, date if adjust else date + datetime.timedelta(days=1)
 
+
+target_csv = input('Input csv file path.\n').replace('"', '')
 
 if os.path.isfile(target_csv):
     with open(target_csv, 'r', encoding='UTF-8') as f:
@@ -33,31 +36,46 @@ if os.path.isfile(target_csv):
             sys.exit()
 
         reader = csv.reader(f)
-        sorted_csv = sorted(reader, key=operator.itemgetter(5))
-        current_ticker = ''
-        total = 0
 
-        for row in sorted_csv:
+        # リストに追加
+        history = [[]]
+        for row in reader:
             ticker = row[5]
-            ticker_root = SetTickerRoot(ticker)
-
-            if current_ticker != ticker_root:
-                pine_code.writelines(f'\nif syminfo.root == "{ticker_root}"\n')
-                total = 0
-
-            d = dt.strptime(row[6], '%Y-%m-%d, %H:%M:%S')
+            ticker_root, d = SetTickerRoot(
+                ticker, dt.strptime(row[6], '%Y-%m-%d, %H:%M:%S'))
             volume = row[7]
             price = row[8]
+
+            if history[0] == []:
+                history[0] = [ticker_root, ticker, d, price, volume]
+            else:
+                history.append([ticker_root, ticker, d, price, volume])
+
+    history.sort(key=operator.itemgetter(0, 2))
+
+    with open('pine.txt', 'w', encoding='UTF-8') as pine_code:
+        current_ticker = ''
+        total = 0
+        for record in history:
+            root = record[0]
+            if current_ticker != root:
+                pine_code.writelines(f'\nif syminfo.root == "{root}"\n')
+                total = 0
+
+            ticker = record[1]
+            year = record[2].year
+            month = record[2].month
+            day = record[2].day
+            price = record[3]
+            volume = record[4]
             total += int(volume)
 
             code = f'{" "*4}'  \
                 'PlotLabel(' \
-                f'{d.year}, {d.month}, {d.day}, {volume}, {total}, {price}, "{ticker}"' \
+                f'{year}, {month}, {day}, {volume}, {total}, {price}, "{ticker}"' \
                 ')\n'
-
             pine_code.writelines(code)
-            current_ticker = ticker_root
-    input('Complete!\nCreated a Pine script in the same location as this python script.')
+            current_ticker = root
 
 elif target_csv == '':
     pass
